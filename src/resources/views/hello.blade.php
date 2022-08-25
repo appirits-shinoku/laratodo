3e2ca7afbf95ccc6043a5cd668a1be67cd7aa4df
daisuke.shinoku <daisuke.shinoku@appirits.com>
2022-08-25 14:16:19 +0900

    HelloController作成

---
+++ src/resources/views/hello.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('contents')
+  <button type="button" class="cursor-pointer m-5 p-3 shadow-md rounded-md font-semibold text-white text-base bg-blue-500 hover:bg-blue-700 ring-blue-200 ring-2">
+    テスト
+  </button>
+@endsection
--- src/resources/views/layout.blade.php
+++ src/resources/views/layout.blade.php
@@ -24,8 +24,6 @@
 
     </head>
     <body class="antialiased">
-        <button type="button" class="cursor-pointer m-5 p-3 shadow-md rounded-md font-semibold text-white text-base bg-blue-500 hover:bg-blue-700 ring-blue-200 ring-2">
-            テスト
-        </button>
+      @yield('contents')
     </body>
 </html>
